a5ec93de12b384c64f4b15f6a2d78406f8582c1d
braaidg <braiandenisgomez@gmail.com>
2022-10-10 22:03:20 -0300

    robo friends with classes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import 'tachyons';
+// import registerServiceWorker from './registerServiceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
