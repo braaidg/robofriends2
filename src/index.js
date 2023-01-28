84b22a4b8e0bb5ae09a3f0204bc53f3c19c3f0a1
Braian Gomez <braiandenisgomez@gmail.com>
2023-01-27 21:11:55 -0300

    Updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
-import './index.css';
-import App from './containers/App';
-import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import "./index.css";
+import App from "./App";
 
-
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
